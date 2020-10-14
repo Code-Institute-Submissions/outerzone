8bc63c741cefafae985376ab7b85b2f524008c5a
H4RP3RK <kirsty.harper@zoho.com>
2020-10-14 19:34:05 +0000

    feat(artists): add artists to views context

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Artist
 
 # Create your views here.
 def index(request):
     """ View returns the index page """
-    return render(request, 'home/index.html')
+    artists = Artist.objects.all()
+    context = {
+        'artists': artists,
+    }
+    return render(request, 'home/index.html', context)
