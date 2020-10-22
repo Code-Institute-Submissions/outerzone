81529531d645df1cd8937e394bef57fcde378e9f
H4RP3RK <kirsty.harper@zoho.com>
2020-10-22 20:31:52 +0000

    feat(basket): add products to basket

--- products/views.py
+++ products/views.py
@@ -17,11 +17,9 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            # *************** this isn't working
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            # ****************
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
