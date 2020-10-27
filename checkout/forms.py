eaefaf0ee3e714ebb07abeaf31745e1ee5e139aa
H4RP3RK <kirsty.harper@zoho.com>
2020-10-27 22:00:56 +0000

    feat(checkout): add forms, admin and signals

---
+++ checkout/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Order
+
+class Orderform(forms.Modelform):
+    class Model:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'postcode', 'county', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        """ Add placeholders and classes, remove auto-generated labels and set autofocus on first field """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name', 
+            'email': 'Email Adress', 
+            'phone_number': 'Phone Number', 
+            'street_address1': 'Postal Address 1', 
+            'street_address2': 'Postal Address 2', 
+            'town_or_city': 'Town or City', 
+            'postcode': 'Postcode', 
+            'county': 'County', 
+            'country': 'Country',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
