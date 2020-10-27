08b4ac36c1bd5b51b761de51e7153d130e3185df
H4RP3RK <kirsty.harper@zoho.com>
2020-10-27 20:26:49 +0000

    feat(checkout): created app and models

--- checkout/models.py
+++ checkout/models.py
@@ -17,12 +17,35 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=99, null=True, blank=True)
     county = models.CharField(max_length=99, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_place=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_place=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_place=2, null=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """ Generates order number """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ Update grand total each time a product is added """
+        self.grand_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """ If order number hasn't already been set, overrides saved method """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.order_number
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max-digits=6, decimal_places=2, null=False, blank=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
+
+    def save(self, *args, **kwargs):
+        """ Overrides original save method to set the lineitem total """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
