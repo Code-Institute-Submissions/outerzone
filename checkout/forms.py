be7e6b91e802bc81b56d571a4e519064ec4b9e95
H4RP3RK <kirsty.harper@zoho.com>
2020-11-01 12:06:02 +0000

    feat(checkout): add html form

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Order
 
-class Orderform(forms.Modelform):
+class OrderForm(forms.ModelForm):
     class Model:
         model = Order
         fields = ('full_name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'postcode', 'county', 'country',)
