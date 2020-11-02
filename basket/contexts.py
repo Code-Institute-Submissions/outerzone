3f03acebd405b84e312b27620a4b72351ca40b6e
H4RP3RK <kirsty.harper@zoho.com>
2020-11-02 21:18:25 +0000

    fix(checkout): adjust basket context to ensure grand total does not equal 0

--- basket/contexts.py
+++ basket/contexts.py
@@ -7,30 +7,26 @@ from products.models import Product
 def basket_contents(request):
 
     basket_items = []
-    total = 0
-    product_count = 0
-    delivery = 3.95
-    basket = request.session.get('basket', {})
     grand_total = 0
+    product_count = 0
+    basket = request.session.get('basket', {})
 
     for item_id, quantity in basket.items():
         product = get_object_or_404(Product, pk=item_id)
         sub_total = quantity * product.price
-        total += sub_total
+        grand_total += sub_total
         product_count += quantity
         basket_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
             'sub_total': sub_total,
-            'total': total,
         })
 
     context = {
         'basket_items': basket_items,
-        'total': total,
         'product_count': product_count,
-        'delivery': delivery,
+        'sub_total': sub_total,
         'grand_total': grand_total,
     }
 
--- checkout/views.py
+++ checkout/views.py
@@ -30,11 +30,15 @@ def checkout(request):
     print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Hj17QGxesLAAhszYHR3zBHIKLklIibhooKQejCARU4QBQwmxkkqWYuxFoe3UTDXBPig7dnQxFXv54AB3K5vs2U300qe2GxUrG',
-        'client_secret': 'test_client_secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
