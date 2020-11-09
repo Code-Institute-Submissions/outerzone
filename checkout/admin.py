f1d3f1d0553a4d0f64a2d5075be86bb029b8d15d
H4RP3RK <kirsty.harper@zoho.com>
2020-11-09 22:27:44 +0000

    feat(checkout): webhook handler complete

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,8 +10,8 @@ class OrderLineItemAdminInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
-    readonly_fields = ('order_number', 'date', 'delivery_cost', 'grand_total',)
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'delivery_cost', 'grand_total',)
+    readonly_fields = ('order_number', 'date', 'delivery_cost', 'grand_total', 'original_basket', 'stripe_pid',)
+    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'delivery_cost', 'grand_total', 'original_basket', 'stripe_pid',)
     list_display = ('order_number', 'date', 'full_name', 'grand_total',)
     ordering = ('-date',)
 
