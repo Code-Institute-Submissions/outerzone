913d51e25727b91a6ecc524967a7b9ebc0c60a31
H4RP3RK <kirsty.harper@zoho.com>
2020-11-24 09:00:22 +0000

    feat(artists): add events model

--- artists/admin.py
+++ artists/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Artist 
+from .models import Artist , Event
 
 # Register your models here.
 class ArtistAdmin(admin.ModelAdmin):
@@ -10,4 +10,14 @@ class ArtistAdmin(admin.ModelAdmin):
     )
     ordering = ('name',)
 
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'artist',
+        'location',
+        'date',
+    )
+    order = ('date',)
+
 admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Event, EventAdmin)
--- artists/models.py
+++ artists/models.py
@@ -10,3 +10,14 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Event(models.Model):
+    artist = models.ForeignKey('Artist', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    location = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
